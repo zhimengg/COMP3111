b348f8006903d1be314411a297e27c3abe7e6cf5
zhimengg <gemmazgao@gmail.com>
2024-02-21 18:46:39 +0800

    Changes output.

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -6,6 +6,6 @@ public class mainApp1 {
         int n = 11;
         System.out.println(b+" to power "+n+" = "+myLibrary.Power(b,n));
         System.out.println(n+"! = "+myLibrary.factorial(n));
-        System.out.println("Program..Ended ...");
+        System.out.println("Program...Ended ...");
     }
 }
