653ad06a21e0ac6c08e84418af927489ae97f863
gimeg <gimeg1008@gmail.com>
2024-02-28 11:38:01 +0800

    Lab 2 part a & b.

---
+++ src/main/java/Lab2b/Book.java
@@ -0,0 +1,32 @@
+package Lab2b;
+import static java.lang.Math.*;
+
+public class Book {
+    private String chapters[];
+    private static final int DEFAULT_CHAPTERS = 10;
+
+    public Book() {
+        chapters = new String[DEFAULT_CHAPTERS];
+        for (int i = 0; i < chapters.length; i++)
+            chapters[i] = "n/a";
+    }
+    public Book(String argument[]) {
+        /* construct the object with an array of chapter names */
+        /* PLEASE ADD YOUR CODE HERE */
+        chapters = new String[argument.length];
+        for (int i = 0; i < argument.length; i++)
+            chapters[i] = argument[i];
+    }
+    public String getChapter(int i) {
+        /* return the chapter by the given index */
+        /* PLEASE ADD YOUR CODE HERE */
+        if (i >= chapters.length) {
+            return "";
+        } else {
+            return chapters[i];
+        }
+    }
+    public String[] getChapters() {
+        return chapters;
+    }
+}
